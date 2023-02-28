6c6933a6da62c6c90e59173b1167d4269091bab9
Aleksei Kudinov <reanim86@yandex.ru>
2023-03-01 00:26:33 +0300

    add some admin panel

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,37 @@
 from django.contrib import admin
 
-from main_app.models import User
+from main_app.models import User, Role, BudjetName, CategoryIncome, Currency, BillsCategory, Bill, Incomes
+
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'role_name']
+
+@admin.register(BudjetName)
+class BudjetNameAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'plan_costs', 'plan_incomes']
+
+@admin.register(CategoryIncome)
+class CategoryIncomeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name_categoryincome', 'plan']
+
+@admin.register(Currency)
+class CurrencyAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name_currency']
+
+@admin.register(BillsCategory)
+class BillsCategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name_billscategory']
+
+@admin.register(Bill)
+class BillAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name_bill', 'summ', 'date', 'bills_category']
+    list_filter = ['date', 'bills_category']
+
+@admin.register(Incomes)
+class IncomesAdmin(admin.ModelAdmin):
+    list_display = ['id', 'creation_date', 'name', 'summ', 'plan', 'permanent_income', 'category', 'currency', 'bill']
+    list_filter = ['creation_date', 'plan', 'permanent_income', 'category', 'currency', 'bill']
+
 
 admin.site.register(User)
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,6 @@
+
+
+import django
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from phonenumber_field.modelfields import PhoneNumberField
@@ -13,8 +16,8 @@ class User(AbstractUser):
     """Пользователи"""
     surname = models.CharField(max_length=40, null=True, blank=True, verbose_name='Фамилия')
     avatar = models.ImageField(null=True, blank=True, verbose_name='Изображение')
-    phone = PhoneNumberField(unique=True, null=False, blank=False, verbose_name='Номер телефона')
-    role = models.ForeignKey(Role, on_delete=models.CASCADE, related_name='users', verbose_name='Роль')
+    phone = PhoneNumberField(null=False, blank=False, verbose_name='Номер телефона')
+    role = models.ForeignKey(Role, on_delete=models.CASCADE, null=True, blank=True, related_name='users')
 
 
 class ConsultantClient(models.Model):
@@ -53,10 +56,10 @@ class Bill(models.Model):
 
 class Incomes(models.Model):
     """Поступление в бюджет"""
-    creation_date = models.DateField(auto_now_add=True, verbose_name='Дата создания')
-    name = models.CharField(max_length=30, verbose_name='Наименование расхода')
-    summ = models.DecimalField(max_digits=19, decimal_places=2, verbose_name='Сумма расхода')
-    description = models.TextField(null=True, blank=True, verbose_name='Описание расхода')
+    creation_date = models.DateField(default=django.utils.timezone.now, verbose_name='Дата создания')
+    name = models.CharField(max_length=30, verbose_name='Наименование поступления')
+    summ = models.DecimalField(max_digits=19, decimal_places=2, verbose_name='Сумма поступления')
+    description = models.TextField(null=True, blank=True, verbose_name='Описание поступления')
     plan = models.BooleanField(default=False, verbose_name='Плановое поступление')
     permanent_income = models.BooleanField(default=False, verbose_name='Постоянный доход')
     category = models.ForeignKey(CategoryIncome, on_delete=models.CASCADE, related_name='incomes')
@@ -68,3 +71,23 @@ class CategoryCost(models.Model):
     name_categorycost = models.CharField(max_length=30, unique=True, verbose_name='Наименование категории расхода ДС')
     plan = models.DecimalField(max_digits=19, decimal_places=2, verbose_name='Планируемое значение расхода')
     description = models.TextField(null=True, blank=True, verbose_name='Описание категории')
+
+class Costs(models.Model):
+    """Затраты"""
+    creation_date = models.DateField(default=django.utils.timezone.now, verbose_name='Дата создания')
+    name = models.CharField(max_length=30, verbose_name='Наименование расхода')
+    summ = models.DecimalField(max_digits=19, decimal_places=2, verbose_name='Сумма расхода')
+    description = models.TextField(null=True, blank=True, verbose_name='Описание расхода')
+    plan = models.BooleanField(default=False, verbose_name='Плановый расход')
+    permanent_cost = models.BooleanField(default=False, verbose_name='Постоянный расход')
+    bound_cost = models.BooleanField(default=False, verbose_name='Обязательный расход')
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE, related_name='costs', verbose_name='Валюта')
+    bill = models.ForeignKey(Bill, on_delete=models.CASCADE, related_name='costs', verbose_name='Счет')
+    category_cost = models.ForeignKey(CategoryCost, on_delete=models.CASCADE, related_name='costs')
+
+class Budget(models.Model):
+    """Бюджет"""
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='budjet')
+    costs = models.ForeignKey(Costs, on_delete=models.CASCADE, related_name='budjet')
+    incomes = models.ForeignKey(Incomes, on_delete=models.CASCADE, related_name='budjet')
+    budjetname_id = models.ForeignKey(BudjetName, on_delete=models.CASCADE, related_name='budjet')
